ae2cd845ce230c3b53911e8b89e5934ff02c7fb1
Nazir-Zuberu <nazir.zuberu@aims-senegal.org>
2023-10-19 17:24:28 -0400

    Parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 import dill 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 
@@ -20,21 +21,29 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param_ = param[ list(models.keys())[i] ]
 
+            # rs = RandomizedSearchCV(model, param_, cv=3)
+            # rs.fit(X_train, y_train)
+
+            gs = RandomizedSearchCV(model, param_, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
+            #y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            #train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
 
